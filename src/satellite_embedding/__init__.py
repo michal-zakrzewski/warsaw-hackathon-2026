b88b99b0d839411a1a0605e17c98bb2175517bf7
pabloknappo <pabliken@gmail.com>
2026-03-28 13:31:45 +0100

    agent sdk

--- src/satellite_embedding/__init__.py
+++ src/satellite_embedding/__init__.py
@@ -3,6 +3,7 @@
 from satellite_embedding.connector import (
     BANDS,
     COLLECTION_ID,
+    compare_years,
     embedding_image,
     init,
     mean_embedding_in_bbox,
@@ -12,6 +13,7 @@ from satellite_embedding.connector import (
 __all__ = [
     "BANDS",
     "COLLECTION_ID",
+    "compare_years",
     "embedding_image",
     "init",
     "mean_embedding_in_bbox",
